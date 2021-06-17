0282de02eb2d747fbdff809f38be909ab92bb740
satya-patel <satyaprakashpatel1998@gmail.com>
2021-06-17 15:28:45 +0530

    servlet to accept valid password

--- src/main/java/com/FirstServlet/LoginServlet.java
+++ src/main/java/com/FirstServlet/LoginServlet.java
@@ -2,6 +2,7 @@ package com.FirstServlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.regex.Pattern;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -12,32 +13,54 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(description = "Login Servlet Testing", urlPatterns = { "/LoginServlet" }, initParams = {
-		@WebInitParam(name = "user", value = "Pallavi"), @WebInitParam(name = "password", value = "Srivastava") })
+		@WebInitParam(name = "user", value = "Satya"), @WebInitParam(name = "password", value = "Satya@121") })
 public class LoginServlet extends HttpServlet {
+	
+	private static final String nameRegex ="^[A-Z]{1}[a-zA-Z]{2}[a-zA-Z]*";
+	private static final String passwordRegex="^(?=.*[@#$%^&+=])(?=.*[0-9])(?=.*[A-Z]).{8,}$";
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
-		String user=req.getParameter("user");
-        String pwd=req.getParameter("pwd");
-        String userID=getServletConfig().getInitParameter("user");
-        String password=getServletConfig().getInitParameter("password");
-        if (userID.equals(user) && password.equals(pwd)) {
-            req.setAttribute("user",user);
-            req.getRequestDispatcher("LoginSuccess.jsp").forward(req,res);
-        }else {
-            RequestDispatcher rd=getServletContext().getRequestDispatcher("/login.html");
-            PrintWriter out=res.getWriter();
-            out.println("<font color=red>Either user name or password is wrong.</font>");
-            rd.include(req,res);
-        }
-    }
-}
-private boolean userFirstName(HttpServletRequest request, HttpServletResponse response, boolean validateUserName)
+		
+		String user = req.getParameter("user");
+		String pwd = req.getParameter("pwd");
+		boolean validateUserName = validateFirstName(user);
+		boolean userFirstName = userFirstName(req, res, validateUserName);
+		boolean validatePassword = validatePassword(pwd);
+		boolean userPassword = userPassword(req, res, validatePassword);
+		String userID = getServletConfig().getInitParameter("user");
+		String password = getServletConfig().getInitParameter("password");
+		if (userFirstName == true &&  userPassword == true) {
+			if (userID.equals(user) && password.equals(pwd)) {
+				req.setAttribute("user", user);
+				req.getRequestDispatcher("LoginSuccess.jsp").forward(req, res);
+			} else {
+				RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.jsp");
+				PrintWriter out = res.getWriter();
+				out.println("<font>Incorrect Credentials</font>");
+				rd.include(req, res);
+			}
+		}
+	}
+
+	private boolean userFirstName(HttpServletRequest request, HttpServletResponse response, boolean validateUserName)
 			throws IOException, ServletException {
 		if (validateUserName == false) {
 			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.jsp");
 			PrintWriter out = response.getWriter();
-			out.println("<font>Incorrect Regex Pattern</font>");
+			out.println("<font>Incorrect Name Regex Pattern</font>");
+			rd.include(request, response);
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean userPassword(HttpServletRequest request, HttpServletResponse response, boolean validatePassword)
+			throws IOException, ServletException {
+		if (validatePassword == false) {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.jsp");
+			PrintWriter out = response.getWriter();
+			out.println("<font>Incorrect Password Regex Pattern</font>");
 			rd.include(request, response);
 			return false;
 		}
@@ -47,6 +70,12 @@ private boolean userFirstName(HttpServletRequest request, HttpServletResponse re
 	public boolean validateFirstName(String userName) {
 		Pattern check = Pattern.compile(nameRegex);
 		boolean value = check.matcher(userName).matches();
+		return value;
+	}
+	
+	public boolean validatePassword(String password) {
+		Pattern check = Pattern.compile(passwordRegex);
+		boolean value = check.matcher(password).matches();
 		return value;
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,6 +1,6 @@
 <html>
 <body>
-<h2>To Login click the link</h2>
+<h2>Click the link here..</h2>
 <a href="login.html">Login Page</a>
 </body>
 </html>
